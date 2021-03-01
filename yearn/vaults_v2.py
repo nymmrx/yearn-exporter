111431be07233144daec9903e199f184a9b82aac
banteg <banteeg@gmail.com>
2021-03-01 17:44:59 +0300

    feat: load v2 vaults from registry

--- yearn/vaults_v2.py
+++ yearn/vaults_v2.py
@@ -2,11 +2,12 @@ from dataclasses import dataclass
 from typing import List
 
 from brownie import interface
-from brownie.network.contract import InterfaceContainer
+from brownie.network.contract import Contract, InterfaceContainer
 from packaging import version
 
 from yearn import strategies
 from yearn import uniswap
+from yearn.events import fetch_events
 from yearn.mutlicall import fetch_multicall
 
 
@@ -46,6 +47,7 @@ VAULT_VIEWS_SCALED = [
 @dataclass
 class VaultV2:
     name: str
+    api_version: str
     vault: InterfaceContainer
     strategies: List[strategies.Strategy]
 
@@ -75,44 +77,22 @@ class VaultV2:
         return info
 
 
-vaults = {
-    "DAI 0.3.0": "0x19D3364A399d251E894aC732651be8B0E4e85001",
-    "USDC 0.3.0": "0x5f18C75AbDAe578b483E5F43f12a39cF75b973a9",
-    "HEGIC 0.3.0": "0xe11ba472F74869176652C35D30dB89854b5ae84D",
-    "curve.fi/steth 0.3.0": "0xdCD90C7f6324cfa40d7169ef80b12031770B4325",
-    "WBTC 0.3.1": "0xcB550A6D4C8e3517A939BC79d0c7093eb7cF56B5",
-    "WETH 0.3.2": "0xa9fE4601811213c340e850ea305481afF02f5b28",
-    "curve.fi/seth 0.3.2": "0x986b4AFF588a109c09B50A03f42E4110E29D353F",
-}
-
-experimental_vaults = {
-    "sUSD Idle 0.3.1": "0x3466c90017F82DDA939B01E8DBd9b0f97AEF8DfC",
-    # https://etherscan.io/address/0xA04fE40eD8a8a8d657E41276ec9e9Ee877675e34#code
-    "WETH Gen Lender 0.3.1": "0x5f18C75AbDAe578b483E5F43f12a39cF75b973a9",
-    # https://etherscan.io/address/0xac5DA2Ca938A7328dE563D7d7209370e24BFd21e#code
-    # "Egyptian God sETH/ETH 0.3.0": "0x0e880118C29F095143dDA28e64d95333A9e75A47",
-    # https://etherscan.io/address/0x3B1a1AE6052ccD643a250fa843c1fB20F9246E1a#code
-    "WETH Iron Lender 0.3.0": "0xED0244B688cF059f32f45E38A6ac6E479D6755f6",
-    # https://etherscan.io/address/0xa35A4972D74d4B3e4486163066E5fFed6d62b213#code
-    "yvSushi YFI-ETH 0.2.2": "0x27Eb83254D900AB4F9b15d5652d913963FeC35e3",
-    # https://etherscan.io/address/0x3213a6389f3f4c287925a47A6D44fe1148FA0C0d#code
-    "DEV Hugger 0.2.2": "0xFeD651936Af7e98F7F2A93c03B1E28a2DA7dfaD4",
-    # https://etherscan.io/address/0x2E949057Ce561BAA9d494895235ACCe310a73FDB#code
-    # https://etherscan.io/address/0x38a97cB34FCE4FAc87D1F7f8639e3341978613b6#code
-    "USDc Idle 0.2.2": "0x33bd0f9618cf38fea8f7f01e1514ab63b9bde64b",
-    # https://etherscan.io/address/0xc29CBe79F1a35a6AA00Df70851E36B14316Ab990#code
-    "Mushroom Worker 0.3.0": "0x0e8A7717A4FD7694682E7005957dD5d7598bF14A"
-    # https://etherscan.io/address/0xE5dc99Cbf841A6721781E592214674A87a1A70BC#code
-    # Left out Lido St. Ether Vault, and ApeTrump Vault
-}
-
-
-def get_vaults():
-    # TODO: read from registry
-    return [VaultV2(name=name, vault=interface.Vault(vault), strategies=[]) for name, vault in vaults.items()]
+def get_vaults(event_key="NewVault"):
+    registry = Contract("v2.registry.ychad.eth")
+    events = fetch_events(registry)
+    versions = {x["api_version"]: Contract(x["template"]).abi for x in events["NewRelease"]}
+    vaults = [
+        Contract.from_abi(f'Vault v{vault["api_version"]}', vault["vault"], versions[vault["api_version"]])
+        for vault in events[event_key]
+    ]
+    symbols = fetch_multicall(*[[x, "symbol"] for x in vaults])
+    names = [f'{name} {vault["api_version"]}' for vault, name in zip(events[event_key], symbols)]
+    return [
+        VaultV2(name=name, api_version=event["api_version"], vault=vault, strategies=[])
+        for name, vault, event in zip(names, vaults, events[event_key])
+    ]
 
 
 def get_experimental_vaults():
-    return [
-        VaultV2(name=name, vault=interface.Vault(vault), strategies=[]) for name, vault in experimental_vaults.items()
-    ]
+    return get_vaults("NewExperimentalVault")
+
